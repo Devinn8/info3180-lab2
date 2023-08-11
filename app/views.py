294e6b72cc48cda6e95f28ebc49ae62c96a42bca
Devinn <devinnweller@yahoo.com>
2023-08-11 13:50:29 -0500

    pics not fully working

--- app/views.py
+++ app/views.py
@@ -2,6 +2,35 @@ from app import app
 from flask import render_template, request, redirect, url_for, flash
 import datetime
 
+# Dictionary to store image information
+uploaded_images = {
+    1: {
+        'filename': '/static/pics/Balloon.jpg',
+        'title': 'Hot Air Balloon',
+        'price': 19999.99,
+        'description': 'Ever wondered what its like to touch the clouds? Well we have the answer. Our air balloon. is a luxurious sight-seeing vehicle. Ensure you do not use it on extremely windy or rainy days'
+    },
+    2: {
+        'filename': 'static/pics/Mist.jpg',
+        'title': 'Misty Forest',
+        'price': 75.00,
+        'description': 'This is quite the conversation starter. We guarantee that your guests will be intrigued by this fascinating image. '
+    },
+    3: {
+        'filename': 'static/pics/Beachball.jpg',
+        'title': 'Beachball',
+        'price': 29.99,
+        'description': 'Need to liven up your next beach or pool trip? Well, take one of our new and improved beachballs with you. Its fun for everyone.'
+    },
+    4: {
+        'filename': 'static/pics/Flowers.jpg',
+        'title': 'Flowers',
+        'price': 14.99,
+        'description': 'Have an important celebration coming up? Want to be extra romantic tonight? Or you just wanna get out of the doghouse? Try using one of our handpicked bouquet of flowers. They should help ;)'
+    }
+   
+}
+
 ###
 # Routing for your application.
 ###
@@ -11,18 +40,22 @@ def home():
     """Render website's home page."""
     return render_template('home.html')
 
-
-@app.route('/about/')
+@app.route('/about')
 def about():
-    """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    """Render the website's about page"""
+    return render_template('about.html')
 
-@app.route('/profile')
-def profile():
-    def format_date_joined():
-        date_joined = datetime.date(2023,2,7)
-        return date_joined.strftime('%b, %Y')
-    return render_template('profile.html', date=format_date_joined())  
+@app.route('/products')
+def products():
+    return render_template('products.html', uploaded_images=uploaded_images)
+
+@app.route('/products/<int:product_id>')
+def view_image(product_id):
+    if product_id in uploaded_images:
+        image_info = uploaded_images[product_id]
+        return render_template('product_view.html', image_info=image_info,  uploaded_images=uploaded_images)
+    else:
+        return "Image not found." 
 
 ###
 # The functions below should be applicable to all Flask apps.
